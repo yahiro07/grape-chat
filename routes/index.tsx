a893af863ab68ed180f9ba6afd56319de96b1954
yahiro <yahiro07@users.noreply.github.com>
2022-12-16 14:41:32 +0900

    initial commit

---
+++ routes/index.tsx
@@ -0,0 +1,25 @@
+import { Head } from "$fresh/runtime.ts";
+import Counter from "../islands/Counter.tsx";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Fresh App</title>
+      </Head>
+      <div>
+        <img
+          src="/logo.svg"
+          width="128"
+          height="128"
+          alt="the fresh logo: a sliced lemon dripping with juice"
+        />
+        <p>
+          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
+          file, and refresh.
+        </p>
+        <Counter start={3} />
+      </div>
+    </>
+  );
+}
